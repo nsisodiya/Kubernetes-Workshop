fe309f271368daaa8726ea8ac30611e8ea715be1
Narendra Sisodiya <narendra.sisodiya.1983@gmail.com>
2020-07-17 13:17:33 +0530

    small change

---
+++ code/1-hello-world-microsvc/README.md
@@ -0,0 +1,19 @@
+# Test Locally
+
+# Step 1- Build the Image
+
+```
+docker build -t helloworld-microsvc .
+```
+
+# Step 2 - Run the container from iamge as Demon.
+
+```
+docker run -d --name helloworld-microsvc-container  -p 3000:3000 helloworld-microsvc
+docker container list -a
+docker logs --follow helloworld-microsvc-container
+```
+
+# Step 3 - Remove Container fully
+
+sudo docker rm -f helloworld-microsvc-container
---
+++ code/1-hello-world-microsvc/main.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+var os = require("os");
+var hostname = os.hostname();
+
+app.get("/", (req, res) => {
+  console.log(`Hello world received a request on Hostname ${hostname}`);
+  const target = process.env.TARGET || "World";
+  res.send(`Hello ${target}! We received a request on Hostname ${hostname}`);
+});
+
+function factorial(n) {
+  var total = 1;
+  for (let index = 1; index <= n; index++) {
+    total = total * index;
+  }
+  return total;
+}
+
+app.get("/factorial/:n", (req, res) => {
+  const n = parseInt(req.params.n); // 'user'
+  res.send(`factorial(${n}) = ${factorial(n)}`);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Hello world listening on port", port);
+});
